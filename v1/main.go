7b166f74d5e1e1828c6565387bce9ce50992fbcb
somkenemuscle <somkeneoj@gmail.com>
2026-05-09 18:24:46 +0100

    Project 4 concluded and commented

--- v1/main.go
+++ v1/main.go
@@ -2,52 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-// 🧪 Project 4 — “Number Analyzer System”
 
-func checkParity(num int) string {
-	if num%2 == 0 {
-		return strconv.Itoa(num) + " is an even number"
-	}
-	return strconv.Itoa(num) + " is an odd number"
+func main()  {
+	fmt.Println("Hello World")
 }
 
-func checkSign(num int) string {
-	if num == 0 {
-		return strconv.Itoa(num) + " is zero"
-	}
-	if num > 0 {
-		return strconv.Itoa(num) + " is positive"
-	}
-	return strconv.Itoa(num) + " is negative"
-}
-
-func compareToHundred(num int) string {
-	if num < 100 {
-		return strconv.Itoa(num) + " is less than 100"
-	}
-	if num > 100 {
-		return strconv.Itoa(num) + " is greater than 100"
-	}
-	return strconv.Itoa(num) + " is equal to 100"
-}
-
-func analyzeNumber(num int) string {
-	parity := checkParity(num)
-	integer := checkSign(num)
-	evaluation := compareToHundred(num)
-	return parity + "\n" + integer + "\n" + evaluation
-
-}
-
-func main() {
-	fmt.Println(analyzeNumber(100))
-	fmt.Println(analyzeNumber(95))
-	fmt.Println(analyzeNumber(0))
-	fmt.Println(analyzeNumber(-40))
-}
 
 // 🧪 Project 1 — “Smart Calculator CLI”
 
@@ -118,4 +79,48 @@ func main() {
 
 // 	return "Login successful"
 
+// }
+
+// 🧪 Project 4 — “Number Analyzer System”
+
+// func checkParity(num int) string {
+// 	if num%2 == 0 {
+// 		return strconv.Itoa(num) + " is an even number"
+// 	}
+// 	return strconv.Itoa(num) + " is an odd number"
+// }
+
+// func checkSign(num int) string {
+// 	if num == 0 {
+// 		return strconv.Itoa(num) + " is zero"
+// 	}
+// 	if num > 0 {
+// 		return strconv.Itoa(num) + " is positive"
+// 	}
+// 	return strconv.Itoa(num) + " is negative"
+// }
+
+// func compareToHundred(num int) string {
+// 	if num < 100 {
+// 		return strconv.Itoa(num) + " is less than 100"
+// 	}
+// 	if num > 100 {
+// 		return strconv.Itoa(num) + " is greater than 100"
+// 	}
+// 	return strconv.Itoa(num) + " is equal to 100"
+// }
+
+// func analyzeNumber(num int) string {
+// 	parity := checkParity(num)
+// 	integer := checkSign(num)
+// 	evaluation := compareToHundred(num)
+// 	return parity + "\n" + integer + "\n" + evaluation
+
+// }
+
+// func main() {
+// 	fmt.Println(analyzeNumber(100))
+// 	fmt.Println(analyzeNumber(95))
+// 	fmt.Println(analyzeNumber(0))
+// 	fmt.Println(analyzeNumber(-40))
 // }
